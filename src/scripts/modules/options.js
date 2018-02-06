58c6d5bbcd769ce04abc29a17548ff99fccb28bc
mz <mattgeoc@autistici.org>
2018-02-06 18:51:50 +0100

    New colors for classification on beeswarm

--- src/scripts/modules/options.js
+++ src/scripts/modules/options.js
@@ -7,10 +7,10 @@ export const color_sup = 'green'; // Color for selected feature with "better" va
 export const color_inf = 'red'; // Color for selected feature with "worse" values than the reference feature
 export const color_highlight = 'yellow'; // Color for the reference feature ("Ma région")
 export const color_default_dissim = '#980043'; // 'darkred'; // Neutral color for selected features when green/red can't be used
-export const color_q4 = '#3b6cb3'; // '#4bac33'; // '#4e8731';//'#ffffb2';
-export const color_q3 = '#3799d3'; // '#4e8731'; // '#4bac33'; //'#fecc5c';
-export const color_q2 = '#fd8d3c';
-export const color_q1 = '#e31a1c';
+export const color_q4 = '#5184bf'; // '#3b6cb3'; // '#4bac33'; // '#4e8731';//'#ffffb2';
+export const color_q3 = '#70bee5'; // '#3799d3'; // '#4e8731'; // '#4bac33'; //'#fecc5c';
+export const color_q2 = '#f3ad7c'; // '#fd8d3c';
+export const color_q1 = '#ec656c'; // '#e31a1c';
 /*
 * Options regarding the formatting of numbers as string
 *
